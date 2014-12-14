284652be7ce4cafa90e4e87d2f621490da45b3bd
Jitendra <Jitendra.zaa@gmail.com>
2014-12-14 02:54:48 -0500

    JayData module for Odata - but it didnt worked

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
 
+ 
 var dbOperations = require("./dbOperations.js");
 var logFmt = require("logfmt");
 
@@ -37,7 +38,7 @@ app.get('/db/dropTable', function(req,res){
 app.set('port', process.env.PORT || 3001);
 
 app.use(express.static(__dirname + '/client')); 
- 
+app.use(express.errorHandler());
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
---
+++ node_modules/jaydata/lib/Types/StorageProviders/Facebook/EntitySets/FQL/friend.js
@@ -0,0 +1,5 @@
+
+$data.Class.define("$data.Facebook.types.FbFriend", $data.Entity, null, {
+    uid1: { type: "number", key: true, searchable: true },
+    uid2: { type: "number", key: true, searchable: true }
+}, null);
